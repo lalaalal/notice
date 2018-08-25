2b182e459c7e68cffe064e5e7e8c108aceb35cd9
lalaalal <100madalala@gmail.com>
2018-08-26 02:15:34 +0900

    off autocomplete

--- body/login.php
+++ body/login.php
@@ -1,8 +1,8 @@
 <div id="content_wrapper">
   <main class="content">
     <form class="" action="#" method="post">
-      <input type="text" name="id" placeholder="아이디를 입력하세요.">
-      <input type="password" name="pw" placeholder="비밀번호를 입력하세요">
+      <input type="text" name="id" placeholder="아이디를 입력하세요." autocomplete="off">
+      <input type="password" name="pw" placeholder="비밀번호를 입력하세요" autocomplete="off">
       <input type="submit" value="로그인">
     </form>
   </main>
