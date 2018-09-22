3a5c30ea2ef8fb7a58ca6451e8818949f293563a
lalaalal <100madalala@gmail.com>
2018-09-22 23:32:31 +0900

    login page

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $mysqli = mysqli_connect("localhost", "notice", "isdj_107", "notice");
-$query = "SELECT name FROM category";
+$query = "SELECT * FROM category";
 $result = $mysqli->query($query);
 
 if (!isset($_GET['page'])) {
@@ -14,23 +14,30 @@ if (!isset($_GET['page'])) {
 <html lang="ko" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <!-- <meta name="viewport" content="width=device-width, user-scalable=no"> -->
     <link rel="stylesheet" href="./css/master.css">
     <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
-    <title></title>
+    <title>7반 알림장</title>
   </head>
   <body>
     <header>
-      <div class="title tile">7반 알림장</div>
+      <div class="title tile"><a href="/">7반 알림장</a></div>
+      <nav>
 <?php
 for ($i = 0; $i < $result->num_rows; $i++) {
   $category = mysqli_fetch_assoc($result);
-  echo "      <span class=\"tile\">{$category['name']}</span>\n";
+  echo "      <span class=\"tile\"><a href=\"?page={$category['id']}\">{$category['name']}</a></span>\n";
 }
 ?>
-      <form class="search" action="#" method="post">
+      </nav>
+      <span class="tile">
+        <img class="login_icon" src="/images/login.svg" alt="login">
+        <a href="?page=login">반장 로그인</a>
+      </span>
+      <!-- <form class="search" action="#" method="post">
         <input class="tile" type="text" name="search" placeholder="검색어를 입력하세요." autocomplete="off">
         <input class="tile" type="submit" value="검색">
-      </form>
+      </form> -->
     </header>
 <?php require("./main/$proc.php") ?>
     <footer>
---
+++ main/login.php
@@ -0,0 +1,13 @@
+<main>
+  <article id="login_wrapper">
+    <div class="notice article_tile">
+      <div class="title"><b>관리자 로그인</b></div>
+    </div>
+    <div class="content article_tile">
+      <b class="title">아이디</b>
+      <input type="text" name="id" placeholder="아이디를 입력하세요." autocomplete="off">
+      <b class="title">비밀번호</b>
+      <input type="password" name="pw" placeholder="비밀번호를 입력하세요.">
+    </div>
+  </article>
+</main>
