c77271397921c91832e6e0eaa6bbc7217768bdd4
lalaalal <100madalala@gmail.com>
2018-10-16 02:03:30 +0900

    .

--- main/schedule.php
+++ main/schedule.php
@@ -1,5 +1,22 @@
 <main>
   <article class="">
-<?php Sorry(); ?>
+    <div class="article_tile head">
+      <b>시간표</b>
+      <div class="content_right">
+        <?= date("Y-m-d") ?>
+      </div>
+    </div>
+    <div class="article_tile content schedule">
+<?php
+$monday = date("Ymd", strtotime(1 - date('w')." days"));
+$friday = date("Ymd", strtotime(6 - date('w')." days"));
+
+$query = "SELECT * FROM board
+          WHERE deadline >= $monday AND deadline <= $friday
+          ORDER BY deadline ASC";
+
+$schedule = json_decode(file_get_contents('https://storage.isdj.ga/schedule/comci.json'), true);
+?>
+    </div>
   </article>
 </main>
